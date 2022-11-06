27834790e6d4fd47436f29347b1ad5c0d1ab2cf8
Ivan Bowen <bowenivan16@gmail.com>
2022-11-06 08:32:38 +0300

    add todo form

---
+++ components/AddToDoForm.js
@@ -0,0 +1,31 @@
+import { send } from "process";
+import { useState } from "react";
+
+
+export const AddTodoForm = () => {
+
+   const [label, setLabel] = useState("");
+   const send = () => {
+      fetch("/api/add-todo", {
+         method : "PUT",
+         headers : {
+            "Content-Type": "application/json"
+         },
+         body: JSON.stringify({
+            label
+         }),
+      }).then(() => window.location.reload());
+   }
+   return (
+      <form onSubmit={(e) => {
+         e.preventDefault();
+         send();
+      }} style={{"display":"flex", "gap": "3em"}}>
+         <label htmlFor="todo">Add Todo: 
+         <input value={label} onChange={(e) => setLabel(e.target.value)} type="text" name="" placeholder='Take out the trash' id="" />
+ 
+         </label>
+         <button>Save</button>
+      </form>
+   );
+}
